4005527b524d5473eea459349aee0d55fa95678b
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-17 06:00:10 -0300

    Finaliza Exercicio

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.WriteLine(@"Um
+     m o v i
+     mento  
+   compondo 
+   além     
+         da 
+  nuvem     
+     um     
+    campo   
+       de   
+    combate
+
+      mira
+    gem
+       ira
+          de
+     um
+        horizonte
+ puro
+    num
+      mo
+      mento
+   vivo
+          Décio pignatari -1956");
+ Console.ResetColor();         
